fbe20400827cd129d03e8ecfe352460061fbe27a
Eric Hall <ericmbf@gmail.com>
2019-03-10 19:12:16 -0300

    Test Foursquare API

--- js/app.js
+++ js/app.js
@@ -1,28 +1,34 @@
+var CLIENT_ID = '5Y0RQGFXWA4VMNA5MPIW5RSFZ2BK0CVH0E4WXDBRYUQ13N2F';
+var CLIENT_SECRET = 'L1SM3BEOGH1K4P05LN1FUHV1WF55XNSJCLVWRVYKSWWBKBTX';
+
 var places = [
     {
         name: "America Airlines Arena",
-        details: "",
-        lat: "",
-        log: "",
+        lat: 25.781685, 
+        long: -80.186908,
+        info: "",
     },
 ]
+
+places.forEach(function(place) {
+  var url = 'https://api.foursquare.com/v2/venues/search?v=20161016&ll=';
+  url += place.lat + ',';
+  url += place.long;
+  url += '&intent=global&query=' + place.title;
+  url += '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET;
+
+  $.getJSON(url).done(function(data) {
+    console.log(data.response.venues[0]);
+  }).fail(function() {
+    console.error('There was an error occured with the Foursquare API. Please try again later.');
+  });
+})
 
 var Place = function (data) { 
 
     this.name = ko.observable(data.name);
     this.details = ko.observable(data.imgSrc);
     this.nicknames = ko.observableArray(data.nicknames);
-    
-    this.level = ko.computed(function () { 
-        if(this.countClick() < 5)
-        {
-            return "level 1";
-        }
-        else 
-        {
-            return "level 2";
-        }
-     }, this);
 }
 
 var ViewModel = function () { 
